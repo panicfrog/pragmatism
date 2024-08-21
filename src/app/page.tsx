e96d9dc244a04c64efc4aea7c2190e93d0c1f6b1
panicfrog-docs <burnedfrog@gmail.com>
2024-08-21 14:06:55 +0800

    modify the font setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-inter text-sm lg:flex">
         <div className="flex flex-col items-center justify-between">
           <h1 className="text-4xl font-bold text-center">
             Welcome to Yongping.me (my personal website)
