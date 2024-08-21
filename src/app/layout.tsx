ebb4e439ebe8da90f5d9d40263f1e55073d9cff9
panicfrog-docs <burnedfrog@gmail.com>
2024-08-21 14:36:51 +0800

    replace the descript for the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css"; // 引入全局 CSS，包含了自定义字体的定义
 
 export const metadata: Metadata = {
-  title: "Yongping",
-  description: "Yongping's personal website",
+  title: "yongping.me",
+  description: "expect the worst, hope the best",
 };
 
 export default function RootLayout({
